74349e45e3ec5df43888e4895149b99228d94bd4
yeongjong310 <tohero21@naver.com>
2021-12-29 15:49:34 +0900

    refactor: result를 관리하는 함수, amount를 계산하는 함수 분리
    1. result 함수를 분리한 이유
     - 함수를 통해 어떤 기능을 하는지 알리기 위함
     - 추 후 result 양식이 변경되면 해당 함수를 호출하여 양식의  유연성을 높이기 위함
    2. amountFor 함수를 분리한 이유
     - 기능 단위로 로직을 분리하기 위고 해당 로직의 실제 역할을 함수 이름을 통해 유추 할 수 있도록 하기 위함

--- performance/statement_refactor_myself.js
+++ performance/statement_refactor_myself.js
@@ -1,7 +1,47 @@
+function resultManager() {
+  let _result = "";
+
+  return {
+    addResultLine(str) {
+      _result += str + "\n";
+    },
+    get result() {
+      return _result;
+    },
+  };
+}
+
+// aPerformance = { playID: 연극 ID, audience: 관람객 수 }
+// play = { ${playID} : { name: 연극이름, type: 장르 }}
+// return totalAmount
+function amountFor(aPerformance, play) {
+  let thisAmount = 0;
+
+  switch (type) {
+    case "tragedy":
+      thisAmount += 40000;
+      if (aPerformance.audience > 30) {
+        thisAmount += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount += 30000;
+      if (aPerformance.audience > 20) {
+        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount;
+}
+
 module.exports = function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let _resultManager = resultManager();
+  _resultManager.addResultLine(`Statement for ${invoice.customer}`);
 
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -11,24 +51,7 @@ module.exports = function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -36,14 +59,14 @@ module.exports = function statement(invoice, plays) {
     // add extra credit for every ten comedy attendees
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
+    _resultManager.addResultLine(
+      ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)`
+    );
     totalAmount += thisAmount;
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  _resultManager.addResultLine(`총액: ${format(totalAmount / 100)}`);
+  _resultManager.addResultLine(`적립 포인트: ${volumeCredits}점`);
 
-  return result;
+  return _resultManager.result;
 };
