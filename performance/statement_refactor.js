90b9807cafacd6cf08e0fe199653e73345ee6c33
yeongjong310 <tohero21@naver.com>
2021-12-29 16:36:33 +0900

    refactor: 함수단위 분리 및 테스트 코드 성공

---
+++ performance/statement_refactor.js
@@ -0,0 +1,54 @@
+module.exports = function statement(invoice, plays) {
+  function amountFor(aPerformance, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount += 40000;
+        if (aPerformance.audience > 30) {
+          thisAmount += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount += 30000;
+        if (aPerformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return thisAmount;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+};
