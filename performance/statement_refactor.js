c15962a857ea1a2e4bf62c7a5d0f5de2d64b5946
yeongjong310 <tohero21@naver.com>
2022-01-01 23:14:30 +0900

    refactor: 변수 인라인하기
    장점
    1. play 임시 변수가 제거되기 떄문에 변수의 역할을 추측할 필요가 없다.
    대신 playFor 함수를 추측하는 것이 필요하다.
    
    단점
    1. 변수에 담아 재활용하지 않기 때문에 playFor 함수를 여러번 호출하게 된다.

--- performance/statement_refactor.js
+++ performance/statement_refactor.js
@@ -38,16 +38,16 @@ module.exports = function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
 
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
