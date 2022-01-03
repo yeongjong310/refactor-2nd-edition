6ee8c62641389cfd7215a8b332b9f0d37fc05c87
yeongjong310 <tohero21@naver.com>
2022-01-03 23:40:46 +0900

    refactor: 문장 슬라이드하기를 이용해 변수 volumeCredits 선언부 이동
    이유: 응집도 up
    변수가 사용되기 직전에 선언할 수 있다면 보다 응집도가 높아지고 코드 이해도도 함께 개선된다.

--- performance/statement_refactor.js
+++ performance/statement_refactor.js
@@ -34,7 +34,6 @@ module.exports = function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   const format = new Intl.NumberFormat("en-US", {
@@ -50,6 +49,7 @@ module.exports = function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditFor(perf);
   }
