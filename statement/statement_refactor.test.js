c72f2484481c57465de4c9ccbb359a236b814668
yeongjong310 <tohero21@naver.com>
2022-01-11 01:25:18 +0900

    test: htmlStatme test 추가

--- statement/statement_refactor.test.js
+++ statement/statement_refactor.test.js
@@ -1,4 +1,4 @@
-const statement = require("./statement_refactor");
+const { statement, htmlStatment } = require("./statement_refactor");
 const invoices = require("./invoices");
 const plays = require("./plays");
 
@@ -12,5 +12,18 @@ describe("statement test", () => {
       "총액: $1,625.00\n" +
       "적립 포인트: 47점\n";
     expect(statement(invoices[0], plays)).toBe(expected);
+  });
+  test("htmlStatment(invoices[0], plays)", () => {
+    let expected =
+      "<h1>청구 내역 (고객명: BigCo)</h1>\n" +
+      "<table>\n" +
+      "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n" +
+      " <tr><td>Hamlet</td><td>55석</td><td>$650.00</td></tr>\n" +
+      " <tr><td>As You Like It</td><td>35석</td><td>$475.00</td></tr>\n" +
+      " <tr><td>Othello</td><td>40석</td><td>$500.00</td></tr>\n" +
+      "</table>\n" +
+      "<p>총액: $1,625.00</p>\n" +
+      "<p>적립 포인트: 47점</p>\n";
+    expect(htmlStatment(invoices[0], plays, true)).toBe(expected);
   });
 });
