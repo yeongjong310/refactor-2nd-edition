831cd5d0d2c8d411710b7fab6f84cb468c48b514
yeongjong310 <tohero21@naver.com>
2022-01-13 16:20:10 +0900

    refactor: 조건부 로직의 수정을 최소화 하기 위해 공연료 계산를 class로 재구현

--- statement/createStatementData.js
+++ statement/createStatementData.js
@@ -36,8 +36,16 @@ module.exports = function createStatementData(invoice, plays) {
 };
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 연극 타입: ${aPlay.type}`);
+  }
 }
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
@@ -45,26 +53,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":
-        result += 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result += 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-
-    return result;
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
   }
 
   get volumeCredit() {
@@ -72,6 +61,28 @@ class PerformanceCalculator {
 
     if ("comedy" === this.play.type)
       result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
     return result;
   }
 }
--- statement/statement_refactor.test.js
+++ statement/statement_refactor.test.js
@@ -7,9 +7,9 @@ describe("statement test", () => {
     let expected =
       "Statement for BigCo\n" +
       " Hamlet: $650.00 (55석)\n" +
-      " As You Like It: $475.00 (35석)\n" +
+      " As You Like It: $580.00 (35석)\n" +
       " Othello: $500.00 (40석)\n" +
-      "총액: $1,625.00\n" +
+      "총액: $1,730.00\n" +
       "적립 포인트: 47점\n";
     expect(statement(invoices[0], plays)).toBe(expected);
   });
@@ -19,10 +19,10 @@ describe("statement test", () => {
       "<table>\n" +
       "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n" +
       " <tr><td>Hamlet</td><td>55석</td><td>$650.00</td></tr>\n" +
-      " <tr><td>As You Like It</td><td>35석</td><td>$475.00</td></tr>\n" +
+      " <tr><td>As You Like It</td><td>35석</td><td>$580.00</td></tr>\n" +
       " <tr><td>Othello</td><td>40석</td><td>$500.00</td></tr>\n" +
       "</table>\n" +
-      "<p>총액: $1,625.00</p>\n" +
+      "<p>총액: $1,730.00</p>\n" +
       "<p>적립 포인트: 47점</p>\n";
     expect(htmlStatment(invoices[0], plays, true)).toBe(expected);
   });
