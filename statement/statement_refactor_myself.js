0f458afee82f1fd72646ee39a5a65fcfbbc4bc02
yeongjong <tohero21@naver.com>
2022-05-26 01:16:09 +0900

    Refactor - play(연극 정보)를 선택하는 질의 함수 추출
    이유
    1. statement에서 play에 대한 정보는 가격계산, 포인트계산, 명세서 작성 등 여러 곳에서 사용되는 데이터다.
    따라서 play를 필요로 하는 내부 함수가 많다.
    질의 함수를 만들게 되면 앞으로 play 인수를 넘겨줄 필요가 없기 때문에 앞으로 만들 함수를 단순화할 수 있다.

--- statement/statement_refactor_myself.js
+++ statement/statement_refactor_myself.js
@@ -1,58 +1,3 @@
-function resultManager() {
-  let _result = "";
-
-  return {
-    addResultLine(str) {
-      _result += str + "\n";
-    },
-    get result() {
-      return _result;
-    },
-  };
-}
-
-function creditsManager() {
-  let _credits = 0;
-
-  return {
-    saveCredits(aPerformance, play) {
-      _credits += Math.max(aPerformance.audience - 30, 0);
-      if ("comedy" === play.type)
-        _credits += Math.floor(aPerformance.audience / 5);
-    },
-    get credits() {
-      return _credits;
-    },
-  };
-}
-
-// aPerformance = { playID: 연극 ID, audience: 관람객 수 }
-// play = { ${playID} : { name: 연극이름, type: 장르 }}
-// return totalAmount
-function amountFor(aPerformance, play) {
-  let thisAmount = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      thisAmount += 40000;
-      if (aPerformance.audience > 30) {
-        thisAmount += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount += 30000;
-      if (aPerformance.audience > 20) {
-        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      thisAmount += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
-  return thisAmount;
-}
-
 module.exports = function statement(invoice, plays) {
   let totalAmount = 0;
   let _creditsManager = creditsManager();
@@ -66,10 +11,10 @@ module.exports = function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    const play = playFor(perf);
+    let thisAmount = amountFor(perf);
 
-    _creditsManager.saveCredits(perf, play);
+    _creditsManager.saveCredits(perf);
 
     _resultManager.addResultLine(
       ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)`
@@ -81,4 +26,67 @@ module.exports = function statement(invoice, plays) {
   _resultManager.addResultLine(`적립 포인트: ${_creditsManager.credits}점`);
 
   return _resultManager.result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function resultManager() {
+    let _result = "";
+
+    return {
+      addResultLine(str) {
+        _result += str + "\n";
+      },
+      get result() {
+        return _result;
+      },
+    };
+  }
+
+  function creditsManager() {
+    let _credits = 0;
+
+    return {
+      saveCredits(aPerformance) {
+        const play = playFor(aPerformance);
+
+        _credits += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === play.type)
+          _credits += Math.floor(aPerformance.audience / 5);
+      },
+      get credits() {
+        return _credits;
+      },
+    };
+  }
+
+  // aPerformance = { playID: 연극 ID, audience: 관람객 수 }
+  // play = { ${playID} : { name: 연극이름, type: 장르 }}
+  // return totalAmount
+
+  function amountFor(aPerformance) {
+    let thisAmount = 0;
+    const play = playFor(aPerformance);
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount += 40000;
+        if (aPerformance.audience > 30) {
+          thisAmount += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount += 30000;
+        if (aPerformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        thisAmount += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return thisAmount;
+  }
 };
